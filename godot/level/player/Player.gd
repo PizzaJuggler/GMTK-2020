02b96bbe1d476caa77c522d50c87e99f0816ee39
Christian Sanders <christiansanders@live.nl>
2020-07-11 00:03:22 +0200

    player can look in constrained direction based on the currently used weapon

--- godot/level/player/Player.gd
+++ godot/level/player/Player.gd
@@ -5,9 +5,19 @@ export var velocity : Vector2
 var look_direction
 # the weapon that is used by the player
 var current_weapon 
+var weapons := []
 
-func ready():
-    pass
+func _ready():
+    obtain_weapons()
+    
+func obtain_weapons():
+    var children = get_children()
+    for child in children:
+        if child.is_in_group('weapon'):
+            weapons.append(child)  
+            
+    if weapons != []:
+        current_weapon = weapons[0]
     
 func change_direction(to : Vector2):
     velocity = to * velocity.length()
@@ -18,9 +28,9 @@ func _process(delta):
 func compute_player_look_direction():
     if current_weapon == null:
         return
-    
-    look_direction = current_weapon.compute_direction(velocity.normalized())
-    look_at(look_direction)
+    look_direction = (get_global_mouse_position() - position)
+    look_direction = current_weapon.compute_constrained_direction(velocity.normalized(), look_direction)
+    look_at(position + look_direction.rotated(PI / 2))
 
 func _physics_process(delta):
     var collision_info = move_and_collide(velocity * delta)
