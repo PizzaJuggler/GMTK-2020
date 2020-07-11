e5004561786b2fc7c676222753aced32b929fb48
sbarrack <12619603+sbarrack@users.noreply.github.com>
2020-07-10 18:10:16 -0700

    bullet self collision

--- godot/level/weapons/Bullet.gd
+++ godot/level/weapons/Bullet.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 var direction setget set_direction
 var velocity
-var speed = 500
+var speed = 200
 
 func set_direction(value):
     direction = value
@@ -11,6 +11,8 @@ func set_direction(value):
 func _physics_process(delta):
     var collision_info = move_and_collide(velocity * delta)
     if collision_info:
+        if collision_info.collider.is_in_group("projectile"):
+            queue_free()
         velocity = velocity.bounce(collision_info.normal)
 
 func _on_LifeTime_timeout():
--- godot/level/weapons/WeaponBase.gd
+++ godot/level/weapons/WeaponBase.gd
@@ -9,38 +9,36 @@ var min_direction : Vector2
 var max_direction : Vector2
 
 func _ready():
-    min_direction = Vector2(-1, 0).rotated(min_rotation)
-    max_direction = Vector2(-1, 0).rotated(max_rotation) 
+	min_direction = Vector2(-1, 0).rotated(min_rotation)
+	max_direction = Vector2(-1, 0).rotated(max_rotation) 
 
 func compute_constrained_direction(move_direction : Vector2, look_direction : Vector2) -> Vector2:
-    var transform_angle = move_direction.angle_to(Vector2(0, -1))
-    var transformed_look = look_direction.rotated(transform_angle)
+	var transform_angle = move_direction.angle_to(Vector2(0, -1))
+	var transformed_look = look_direction.rotated(transform_angle)
 
-    if _is_between_allowed_directions(transformed_look):
-        return look_direction
-    else:
-        return _get_closest_direction(transformed_look).rotated(transform_angle)
-        
-
-
+	if _is_between_allowed_directions(transformed_look):
+		return look_direction
+	else:
+		return _get_closest_direction(transformed_look).rotated(transform_angle)
+		
 func _is_between_allowed_directions(move_direction : Vector2) -> bool:
-    var cross_min_max = min_direction.cross(max_direction)
-    if cross_min_max >= 0:
-        # is in 0...180 range
-        return min_direction.cross(move_direction) >= 0 and move_direction.cross(max_direction) >= 0
-    else:
-        # is in 180...360 range
-        return not (max_direction.cross(move_direction) >= 0 and move_direction.cross(min_direction) >= 0)
-    
+	var cross_min_max = min_direction.cross(max_direction)
+	if cross_min_max >= 0:
+		# is in 0...180 range
+		return min_direction.cross(move_direction) >= 0 and move_direction.cross(max_direction) >= 0
+	else:
+		# is in 180...360 range
+		return not (max_direction.cross(move_direction) >= 0 and move_direction.cross(min_direction) >= 0)
+	
 func _get_closest_direction(move_direction : Vector2) -> Vector2:
-    var angle_max = abs(move_direction.angle_to(max_direction))
-    var angle_min = abs(move_direction.angle_to(min_direction))
-    if angle_max > angle_min:
-        return min_direction
-    else:
-        return max_direction
+	var angle_max = abs(move_direction.angle_to(max_direction))
+	var angle_min = abs(move_direction.angle_to(min_direction))
+	if angle_max > angle_min:
+		return min_direction
+	else:
+		return max_direction
 
 func fire(look_direction : Vector2):
-    var bullet = bullet_type.instance()
-    bullet.direction = look_direction
-    return bullet
+	var bullet = bullet_type.instance()
+	bullet.direction = look_direction
+	return bullet
