6239fa485c7a93a5835cc938dfe6fea8db8cdc50
Christian Sanders <christiansanders@live.nl>
2020-07-11 17:27:01 +0200

    added coins to hud

--- godot/gui/HUD/HUD.gd
+++ godot/gui/HUD/HUD.gd
@@ -1,10 +1,12 @@
 extends Control
 
-var hearts = []
+var hearts : Array
+onready var coin_label = find_node("CoinLabel")
 
 func _ready():
     hearts = find_node("HeartContainer").get_children()
     set_hearts(1.5)
+    set_coins(200)
 
 func set_hearts(life : float):
     for heart in hearts:   
@@ -16,3 +18,6 @@ func set_hearts(life : float):
             heart.empty()
             
         life -= 1.0
+
+func set_coins(coins : int):
+    coin_label.text = str(coins)
